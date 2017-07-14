e30fb5afd6d809931156879a8d1a61978efcd15c
tedlopez <tedmlopez@gmail.com>
2017-07-14 19:54:28 -0400

    CSS edits

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,32 +1,25 @@
 class TodoListsController < ApplicationController
   before_action :set_todo_list, except: [:new, :create]
-  # GET /todo_lists
-  # GET /todo_lists.json
+
   def index
     @todo_lists = TodoList.all
   end
 
-  # GET /todo_lists/1
-  # GET /todo_lists/1.json
   def show
     @user = current_user
     @todo_list = TodoList.find(params[:id])
   end
 
-  # GET /todo_lists/new
   def new
     @user = current_user
     @todo_list = TodoList.new
   end
 
-  # GET /todo_lists/1/edit
   def edit
     @user = current_user
     @todo_list = TodoList.find(params[:id])
   end
 
-  # POST /todo_lists
-  # POST /todo_lists.json
   def create
     @todo_list = TodoList.new(todo_list_params)
     @todo_list.user = current_user
@@ -40,8 +33,6 @@ class TodoListsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /todo_lists/1
-  # PATCH/PUT /todo_lists/1.json
   def update
     @user = current_user
     @todo_list = TodoList.find(params[:id])
@@ -49,15 +40,13 @@ class TodoListsController < ApplicationController
 
     if @todo_list.save
       flash[:notice] = "\"#{@todo_list.title}\" has been updated."
-      redirect_to @todo_list
+      redirect_to user_todo_list_path(@user, @todo_list)
     else
       flash.now[:alert] = "There was an error updating the entry. Please try again."
       render :edit
     end
   end
 
-  # DELETE /todo_lists/1
-  # DELETE /todo_lists/1.json
   def destroy
     @user = current_user
     @todo_list = TodoList.find(params[:id])
@@ -70,12 +59,11 @@ class TodoListsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_todo_list
+      @user = current_user
       @todo_list = TodoList.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def todo_list_params
       params.require(:todo_list).permit(:title, :description, :user_id)
     end
