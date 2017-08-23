483570388b59489c63ab05619a91e7915d676bcd
tedlopez <tedmlopez@gmail.com>
2017-08-23 15:50:04 -0400

    Added blocmetrics to application.js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,20 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+var blocmetrics = {};
+
+blocmetrics.report = function(eventName){
+
+  // #1
+var event = {event: { name: eventName }};
+
+// #2
+var request = new XMLHttpRequest();
+// #3
+request.open("POST", "http://localhost:3000/api/events", true);
+// #4
+request.setRequestHeader('Content-Type', 'application/json');
+// #5
+request.send(JSON.stringify(event));
+};
