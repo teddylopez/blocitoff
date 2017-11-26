0cc06bde52a9b5994aaf324009549a9da1a426fc
teddylopez <tedmlopez@gmail.com>
2017-11-26 18:31:32 -0500

    Production changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,16 +27,14 @@ $(document).ready(function(){
 var blocmetrics = {};
 
 blocmetrics.report = function(eventName){
-
   // #1
-var event = {event: { name: eventName }};
-
-// #2
-var request = new XMLHttpRequest();
-// #3
-request.open("POST", "http://localhost:3000/api/events", true);
-// #4
-request.setRequestHeader('Content-Type', 'application/json');
-// #5
-request.send(JSON.stringify(event));
+  var event = {event: { name: eventName }};
+  // #2
+  var request = new XMLHttpRequest();
+  // #3
+  request.open("POST", "http://localhost:3000/api/events", true);
+  // #4
+  request.setRequestHeader('Content-Type', 'application/json');
+  // #5
+  request.send(JSON.stringify(event));
 };
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,14 +80,14 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'https://morning-coast-28495.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-     :address              => "smtp.gmail.com",
-     :port                 => 587,
-     :domain               => 'heroku.com',
-     user_name:            ENV["SENDGRID_USERNAME"],
-     password:             ENV["SENDGRID_PASSWORD"],
-     :authentication       => 'plain',
-     :enable_starttls_auto => true
- }
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
 end
