b321f6991635786a6068a0237adf3cf1e8ff89bd
teddylopez <tedmlopez@gmail.com>
2017-11-27 13:12:54 -0500

    Modifying production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'https://morning-coast-28495.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://aqueous-temple-18135.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
