c5ce15a842511027f411a6b4203fe62229577d79
teddylopez <tedmlopez@gmail.com>
2018-02-23 20:48:54 -0500

    Minor changes.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,21 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+
+var blocmetrics = {};
+
+blocmetrics.report = function(eventName){
+  // #1
+  var event = {event: { name: eventName }};
+  console.log(event);
+  // #2
+  var request = new XMLHttpRequest();
+  // #3
+  request.open("POST", "http://localhost:3000/api/events", true);
+  // #4
+  request.setRequestHeader('Content-Type', 'application/json');
+  // #5
+  console.log(JSON.stringify(event));
+
+  request.send(JSON.stringify(event));
+};
